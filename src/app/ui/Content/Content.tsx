2dda34b7b2c3e47dd85bdbb8e3c13575e910e28a
Ahmed Mamdouh <medoduh@gmail.com>
2024-04-02 12:50:59 +0200

    - corrected typo - Removed forced light mode

--- src/app/ui/Content/Content.tsx
+++ src/app/ui/Content/Content.tsx
@@ -20,7 +20,7 @@ function Content () {
             <Card>
                 <p className="content-text text-primary">
                     In 2014, I was curious about combinatorics and algorithms.
-                    I started reading about combinatorics as well as well as learning Python by reading the documentation.
+                    I started reading about combinatorics as well as learning Python by reading the documentation.
                     Today, I have a bachelor in computer science, I am experienced in most common programming domains.
                     I also study the artificial intelligence at Fredrich-Alexander-Universität, Erlangen, Germany
                 </p>
