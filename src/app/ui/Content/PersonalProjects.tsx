4531de6c50e9bbe0b1a7e65670ba02a3a0791fcd
Ahmed Mamdouh <medoduh@gmail.com>
2024-04-17 17:53:27 +0200

    quick fix

--- src/app/ui/Content/PersonalProjects.tsx
+++ src/app/ui/Content/PersonalProjects.tsx
@@ -2,12 +2,12 @@ import useIntersectionObserver from "@/app/hooks/useIntersectionObserver";
 import Icon from "../Icon";
 import "./personalProjects.css";
 import AnimatedScrollCard from "@/app/ui/AnimatedScrollCard/AnimatedScrollCard";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { useEffect } from "react";
 import { VisDataSetter } from "@/app/types";
 
 const ProjectCard = ({title, children, githubLink}:Readonly<{title: string, children: React.ReactNode, githubLink: string}>) => {
     return (
-        <div className="w-full border-solid border border-[var(--primary)] max-w-[40rem] p-5 m-auto flex flex-col">
+        <div className="w-full border-solid border border-[var(--primary)] max-w-[40rem] p-5  my-2 flex flex-col">
             <div className="flex flex-row justify-center items-center w-full max-h-[50px]">
                 <p className="text-2xl text-[var(--secondary)]"> {title} </p>
                 
@@ -19,29 +19,25 @@ const ProjectCard = ({title, children, githubLink}:Readonly<{title: string, chil
                 </a>
             </div>
         </div>
-
     );
 };
-
 
 function PersonalProjects({visSetter}: {visSetter: VisDataSetter}) {
     const [intersection, ref] = useIntersectionObserver({slices: 100});
     useEffect(() => { visSetter(intersection); }, [intersection, visSetter]);
     return (
-        <AnimatedScrollCard reff={ref} id="projects" className="flex flex-col items-center content-center p-10">
-            <p className="text-4xl flex mb-5 text-primary">Personal passions</p>
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <ProjectCard title="Eden" githubLink="https://github.com/a-mamdouh/Eden">
-                    I am working on a platform independent graphics engine using C++.
-                    Eden is built to be extensible and hackable by developers for devlopers.
-                    I am focusing on testing, performance and easy of use. Check it out
-                </ProjectCard>
+        <AnimatedScrollCard reff={ref} id="projects" className="p-10">
+            <p className="text-4xl flex mb-8 text-primary">Personal passions</p>
+            <ProjectCard title="Eden" githubLink="https://github.com/a-mamdouh/Eden">
+                I am working on a platform independent graphics engine using C++.
+                Eden is built to be extensible and hackable by developers for devlopers.
+                I am focusing on testing, performance and easy of use. Check it out
+            </ProjectCard>
 
-                <ProjectCard title="Bonk Patrol" githubLink="https://github.com/a-mamdouh/bonk-patrol">
-                    This is a simple, lightweight voice chat time-out Discord bot I developed using the JavaScript Discord API.
-                    I also added a feature for message clearing messages
-                </ProjectCard>
-            </div>
+            <ProjectCard title="Bonk Patrol" githubLink="https://github.com/a-mamdouh/bonk-patrol">
+                This is a simple, lightweight voice chat time-out Discord bot I developed using the JavaScript Discord API.
+                I also added a feature for message clearing messages
+            </ProjectCard>
         </AnimatedScrollCard>
     );
 }
--- src/app/ui/Content/WorkExperience.tsx
+++ src/app/ui/Content/WorkExperience.tsx
@@ -1,40 +1,37 @@
 import useIntersectionObserver from "@/app/hooks/useIntersectionObserver";
 import "./workExperience.css";
 import AnimatedScrollCard from "@/app/ui/AnimatedScrollCard/AnimatedScrollCard";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { useEffect } from "react";
 import { VisDataSetter } from "@/app/types";
 
 const WorkExperienceCard = ({title, children}:Readonly<{title: string, children: React.ReactNode}>) => {
     return (
-        <div className="w-full border-solid border border-[var(--primary)] max-w-[40rem] p-5 mb-2 m-auto">
+        <div className="w-full border-solid border border-[var(--primary)] max-w-[40rem] p-5 flex flex-col my-2">
             <p className="text-2xl text-[var(--secondary)]">{title}</p>
-            <p>
-                {children}
-            </p>
+            <div className="w-full flex flex-row justify-center items-center p-2">
+                <p> {children} </p>
+            </div>
         </div>
 
     );
 };
 
-
 function WorkExperience({visSetter}: {visSetter: VisDataSetter}) {
     const [intersection, ref] = useIntersectionObserver({slices: 100});
     useEffect(() => { visSetter(intersection); }, [intersection, visSetter]);
     return (
-        <AnimatedScrollCard reff={ref} id="experience" className="flex flex-col items-center content-center p-10">
-            <p className="text-4xl flex mb-5 text-primary">Problem solver at heart</p>
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <WorkExperienceCard title="Primetals Technologies">
-                    I worked on different projects involving state of the art deep learning models.
-                    I also developed efficient, real-time software solutions for different problems,
-                    as well as new internal development tools such as project templates.
-                </WorkExperienceCard>
-                <WorkExperienceCard title="Sequel Solutions">
-                    I implemented a new search feature that worked with weather data to find an appropriate location/time.
-                    I also created a Firebase caching service and integrated it into the application.
-                    I also updated critical, deprecated parts of the legacy code to boost performance and reduce loading times by up to 30%
-                </WorkExperienceCard>
-            </div>
+        <AnimatedScrollCard reff={ref} id="experience" className="p-10">
+            <p className="text-4xl flex mb-8 text-primary">Problem solver at heart</p>
+            <WorkExperienceCard title="Primetals Technologies">
+                I worked on different projects involving state of the art deep learning models.
+                I also developed efficient, real-time software solutions for different problems,
+                as well as new internal development tools such as project templates.
+            </WorkExperienceCard>
+            <WorkExperienceCard title="Sequel Solutions">
+                I implemented a new search feature that worked with weather data to find an appropriate location/time.
+                I also created a Firebase caching service and integrated it into the application.
+                I also updated critical, deprecated parts of the legacy code to boost performance and reduce loading times by up to 30%
+            </WorkExperienceCard>
         </AnimatedScrollCard>
     );
 }
