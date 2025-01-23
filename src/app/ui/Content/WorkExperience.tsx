61e1e7b987513f81bae5da983796c06867668cbb
Ahmed Mamdouh <medoduh@gmail.com>
2025-01-23 20:29:35 +0100

    update work experience

--- src/app/ui/Content/WorkExperience.tsx
+++ src/app/ui/Content/WorkExperience.tsx
@@ -25,15 +25,18 @@ function WorkExperience({visSetter}: {visSetter: VisDataSetter}) {
     return (
         <AnimatedScrollCard reff={ref} id="experience" className="p-10">
             <p className="title-text flex mb-8 text-primary">Problem solver at heart</p>
-            <WorkExperienceCard title="Primetals Technologies" dates="Apr. 23 - Now">
+            <WorkExperienceCard title="SAP Fioneer" dates="Oct. 24 - Now">
+                Powering banking and insurance with the Financial Product Subledger (FPSL).
+                Working on enterprise-level projects in interwoven teams with the help of customers to super charge the financial industry
+            </WorkExperienceCard>
+            <WorkExperienceCard title="Primetals Technologies" dates="Apr. 23 - Sep. 24">
                 I worked on different projects involving state of the art deep learning models.
                 I also developed efficient, real-time software solutions for different problems,
                 as well as new internal development tools such as project templates.
             </WorkExperienceCard>
             <WorkExperienceCard title="Sequel Solutions" dates="Sep. 20 - Jul. 21">
-                I implemented a new search feature that worked with weather data to find an appropriate location/time.
-                I also created a Firebase caching service and integrated it into the application.
-                I also updated critical, deprecated parts of the legacy code to boost performance and reduce loading times by up to 30%
+                I Revamped application core services to boost performance by 30%.
+                I also worked on a new feature using weather data and weather prediction for weather-based vacation planning
             </WorkExperienceCard>
         </AnimatedScrollCard>
     );
