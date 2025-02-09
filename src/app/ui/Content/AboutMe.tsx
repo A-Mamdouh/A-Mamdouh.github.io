7cb20c1533d0b6687ce3077a1fb028dafb9bafcb
Ahmed Mamdouh <medoduh@gmail.com>
2025-02-09 10:24:18 +0100

    fixed apostrophe error

--- src/app/ui/Content/AboutMe.tsx
+++ src/app/ui/Content/AboutMe.tsx
@@ -12,8 +12,8 @@ function AboutMe ({visSetter}: {visSetter: VisDataSetter}) {
             <p className="about-me text-primary big-text">
             My journey into programming began in 2014 with a deep curiosity about combinatorics and algorithms.
             I started by exploring combinatorics while teaching myself Python through its documentation.
-            Over the years, I earned a bachelor's degree in computer science and gained experience across various programming domains.
-            I have since completed my master's in artificial intelligence at Friedrich-Alexander-Universität Erlangen and
+            Over the years, I earned a bachelor&apos;s degree in computer science and gained experience across various programming domains.
+            I have since completed my master&apos;s in artificial intelligence at Friedrich-Alexander-Universität Erlangen and
             now work as an ABAP developer at SAP Fioneer, where I apply my expertise to enterprise software solutions.
             </p>
         </AnimatedScrollCard>
